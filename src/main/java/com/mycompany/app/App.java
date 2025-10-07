173cafc8aeb8e9e251a0d0092df09065ce46e9f1
Sharanya Katna <sharanyakatna@Sharanyas-MacBook-Air.local>
2025-10-07 13:10:35 +0530

    Update message for CI/CD test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello from Sharanya's CI/CD!";
 
     public App() {}
 
@@ -17,3 +17,4 @@ public class App {
         return MESSAGE;
     }
 }
+
